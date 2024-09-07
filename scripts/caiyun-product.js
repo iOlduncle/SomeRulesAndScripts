996f90dc723f63ea2c0b069ab0fc7475580527ec
iOlduncle <8361027+iOlduncle@users.noreply.github.com>
2024-09-07 15:41:59 +0800

    modified:   modules/caiyun-test.sgmodule 	new file:   scripts/caiyun-product.js

---
+++ scripts/caiyun-product.js
@@ -0,0 +1,9 @@
+console.log($request.url);
+console.log($response.body);
+var body = JSON.parse($response.body);
+
+if ($request.url.indexOf(product) != -1){
+    body.pay=[];
+}
+
+$done({ body: JSON.stringify(body)});
