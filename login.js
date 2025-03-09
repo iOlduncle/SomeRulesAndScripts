5b21c1675ba231a0e1e4ede594e53d3dce2f70f3
iOlduncle <8361027+iOlduncle@users.noreply.github.com>
2025-03-09 17:41:56 +0800

    modified:   tmp/server.json

---
+++ login.js
@@ -0,0 +1,3 @@
+window['setServiceListdUrl'] = 'https://raw.githubusercontent.com/iOlduncle/SomeRulesAndScripts/refs/heads/main/tmp/server.json';
+window['serviceListdUrl'] = 'https://raw.githubusercontent.com/iOlduncle/SomeRulesAndScripts/refs/heads/main/tmp/server.json';
+start();
